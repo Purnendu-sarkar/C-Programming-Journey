323199401777d3aa7afcf724e0dde4c4757d1cb6
Purnendu-sarkar <dsr102.purnendu@gmail.com>
2025-04-01 14:44:21 +0600

    Initial commit - Added basic input-output program in C

---
+++ Formate Specifier/Second.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+
+int main() {
+    int a;
+
+    scanf("%d", &a); // %d is the format specifier for int
+
+    printf("%d\n", a); // %d is the format specifier for int
+
+    return 0;
+
+}
+
+// int main() {
+//     long long int a;
+
+//     scanf("%lld", &a); // %d is the format specifier for int
+
+//     printf("%lld\n", a); // %d is the format specifier for int
+
+//     return 0;
+
+// }
+
+// int main() {
+//     int a;
+
+//     scanf("%d", &a);
+
+//     printf("%d\n", &a); // &a is the memory address of a
+
+//     return 0;
+
+// }
+
+// int main() {
+//     int a;
+
+//     scanf("%d", &a);
+
+//     printf("%d\n", &a); // &a is the memory address of a
+
+//     return 0;
+
+// }
+
+/*
+Formate Specifier:
+
+short int: %hd
+int: %d
+long int: %ld
+long long int: %lld
+
+char: %c
+
+float: %f
+double: %lf
+long double: %Lf
+
+unsigned int: %u
+unsigned short int: %hu
+unsigned long int: %lu
+unsigned long long int: %llu
+*/
