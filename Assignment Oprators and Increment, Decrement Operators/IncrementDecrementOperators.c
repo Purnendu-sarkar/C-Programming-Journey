35e829d8cb436e8e6f08b36a88426dcfcb68efea
Purnendu-sarkar <dsr102.purnendu@gmail.com>
2025-04-02 15:43:59 +0600

    Increment And Decrement Operators

---
+++ Assignment Oprators and Increment, Decrement Operators/IncrementDecrementOperators.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<math.h>
+
+int main() {
+    int a = 50; // Initialize a with 50
+
+    printf("Initial value of a: %d\n", a); // Print the initial value of a
+
+    a++; // Increment a by 1
+
+    printf("After incrementing, a: %d\n", a); // Print the incremented value of a
+
+    return 0;
+}
+
+//Text 02
+
+// int main() {
+//     int a = 50;
+
+//     printf("value of a: %d\n", a--);
+
+
+//     return 0;
+// }
+
+
+//Text 03
+// int main() {
+//     int a = 50;
+
+//     printf("value of a: %d\n", --a);
+
+
+//     return 0;
+// }
