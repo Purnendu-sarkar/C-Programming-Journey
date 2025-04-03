909510556c9871175200a33718a9e30ad3baa2d6
Purnendu-sarkar <dsr102.purnendu@gmail.com>
2025-04-03 14:57:36 +0600

    Conditional Statements => if

---
+++ Conditional Statements/if/if.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 10, b = 50;
+    
+    printf("Cheeking if a is greater than b\n");
+    
+    if (a > b)
+    {
+        printf("a is greater than b\n");
+    }
+    printf("a is not greater than b\n");
+
+    return 0;
+}
